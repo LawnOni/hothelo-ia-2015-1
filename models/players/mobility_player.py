2363663b4dc011fa3a314f9a123b0a2eb1132394
Luan Martins <lawncm@gmail.com>
2015-06-18 20:51:17 -0300

    Começo da abordagem alpha e beta

--- models/players/mobility_player.py
+++ models/players/mobility_player.py
@@ -12,14 +12,16 @@ class MobilityPlayer:
             return Board.BLACK
 
     def play(self, board):
-        minimax = self.max(board, 2)
+        alpha = -float('inf')
+        beta = float('inf')
+        minimax = self.max(board, 2, alpha, beta)
         return minimax[0]
         
     def getBestMove(self, board, color):
-        best_value = -99
+        best_value = -float('inf')
         moves = board.valid_moves(color)
         retMove = None
-        movesLength = 99
+        movesLength = float('inf')
         for move in moves:
             board_clone = board.get_clone()
             board_clone.play(move,color)
@@ -32,30 +34,39 @@ class MobilityPlayer:
                 
         return retMove, (moves.__len__() - movesLength)
         
-    def max(self, board, depth):
+    def max(self, board, depth, alpha, beta):
         if depth == 0:
             return self.getBestMove(board, self.color)
                 
         moves = board.valid_moves(self.color)
 
         if moves.__len__() == 0:
-            return None, 99
+            return None, float('inf')
 
-        best_value = -99
+        best_value = -float('inf')
+
         retMove = None
         for move in moves:
+            #import pdb; pdb.set_trace()
             board_clone = board.get_clone()
             board_clone.play(move,self.color)
-            min_move, min_value = self.min(board_clone, depth-1)
+            min_value = self.min(board_clone, depth-1, alpha, beta)[1]
+
             if min_value >= best_value:
                 best_value = min_value
                 retMove = move
+
+            alpha = max(best_value, alpha)
+            #   import pdb; pdb.set_trace()
+            if alpha > beta:
+                #import pdb; pdb.set_trace()
+                return None, float('inf')
 
         return retMove, best_value
             
             
             
-    def min(self, board, depth):       
+    def min(self, board, depth, alpha, beta):       
 
         if depth == 0:
             return self.getBestMove(board, self.opponent_color)
@@ -63,17 +74,25 @@ class MobilityPlayer:
         moves = board.valid_moves(self.opponent_color)
 
         if moves.__len__() == 0:
-            return None, -99
+            return None, -float('inf')
 
-        best_value = 99
+        best_value = float('inf')
         retMove = None
         
         for move in moves:
+            # import pdb; pdb.set_trace()
             board_clone = board.get_clone()
             board_clone.play(move,self.opponent_color)
-            max_move, max_value = self.max(board_clone, depth-1)
+            max_value = self.max(board_clone, depth-1, alpha, beta)[1]
+
             if max_value <= best_value:
                 best_value = max_value
                 retMove = move
+
+            beta = min(best_value, beta)
+
+            if alpha > beta:
+                #import pdb; pdb.set_trace()
+                return None, -float('inf')
 
         return retMove, best_value
